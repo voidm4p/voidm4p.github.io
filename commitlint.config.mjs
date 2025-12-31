59d2e8c629260a50e2a5464ccf9e5792ffc16aba
MPV <voidm4p@protonmail.com>
2025-12-31 20:42:04 +0100

    Remove commit lint check

--- commitlint.config.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    // keep body lines unbounded to match existing package.json config
-    'body-max-line-length': [0, 'always']
-  }
-};
